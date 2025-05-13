7ec286c6a184b9d2be2559c10c9cd113f6e8b05d
Nguyen Minh Tho <208221158+tobynguyen27@users.noreply.github.com>
2025-05-13 20:54:34 +0700

    Setup Vercel speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { RouterProvider, createRouter } from "@tanstack/react-router"
 import { routeTree } from "./routeTree.gen"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 import "./styles/globals.css"
 import "virtual:uno.css"
@@ -16,5 +17,6 @@ declare module "@tanstack/react-router" {
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<RouterProvider router={router} />
+		<SpeedInsights />
 	</StrictMode>,
 )
