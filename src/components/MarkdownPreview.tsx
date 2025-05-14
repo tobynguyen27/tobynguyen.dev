7022cc1840f15afd992d308e247d5e7a46e12543
Nguyen Minh Tho <208221158+tobynguyen27@users.noreply.github.com>
2025-05-14 18:25:25 +0700

    Update code highlighter

--- src/components/MarkdownPreview.tsx
+++ src/components/MarkdownPreview.tsx
@@ -1,5 +1,7 @@
-import { lazy } from "react"
+import { lazy, ReactNode } from "react"
 import "@/styles/markdown.css"
+import ShikiHighlighter, { Element, isInlineCode } from "react-shiki"
+
 interface MarkdownPreviewProps {
 	source: string
 }
@@ -7,5 +9,34 @@ interface MarkdownPreviewProps {
 const Markdown = lazy(() => import("react-markdown"))
 
 export default function MarkdownPreview({ source }: MarkdownPreviewProps) {
-	return <Markdown children={source} />
+	return <Markdown children={source} components={{ code: CodeHighlight }} />
+}
+
+function CodeHighlight({
+	className,
+	children,
+	node,
+	...props
+}: {
+	className?: string
+	children?: ReactNode
+	node?: Element
+}) {
+	const code = String(children).trim()
+	const match = className?.match(/language-(\w+)/)
+	const language = match ? match[1] : undefined
+	const isInline = node ? isInlineCode(node) : undefined
+
+	return !isInline ? (
+		<ShikiHighlighter
+			language={language}
+			theme='catppuccin-mocha'
+			{...props}>
+			{code}
+		</ShikiHighlighter>
+	) : (
+		<code className={className} {...props}>
+			{code}
+		</code>
+	)
 }
