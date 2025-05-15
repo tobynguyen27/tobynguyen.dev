d6ee80f7fc31244d369c9d4e714faf7c78cadbbb
Nguyen Minh Tho <208221158+tobynguyen27@users.noreply.github.com>
2025-05-15 16:17:31 +0700

    Remove unused stuff

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute, Link } from "@tanstack/react-router"
+import { createFileRoute } from "@tanstack/react-router"
 import Badge from "@/components/Badge"
 
 export const Route = createFileRoute("/")({
