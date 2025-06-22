f5d39eda29ebf5a6600bb91bb1cc0c392ddddb58
Nguyen Minh Tho <208221158+tobynguyen27@users.noreply.github.com>
2025-06-22 11:06:00 +0700

    Update theme

--- uno.config.ts
+++ uno.config.ts
@@ -11,10 +11,15 @@ import {
 
 export default defineConfig({
   shortcuts: {
-    "text-base": "text-gray-900 dark:text-gray-300",
-    "subtext-base": "text-gray-600/85 dark:text-gray-300/85",
-    "text-link": "text-base hover:(underline underline-offset-3 text-gray-900) text-gray-600 dark:text-gray-300 dark:hover:text-white",
-    "navbar-item-text": "text-gray-600 hover:text-gray-900 dark:text-gray-300 dark:hover:text-white",
+    "navbar-item-text": "text-secondary hover:text-primary",
+    "bg-2": "bg-[#F9FAFB] dark:bg-black",
+    "bg-1": "bg-white dark:bg-[#0A0A0A]",
+    "text-primary": "text-[#171717] dark:text-[#EDEDED]",
+    "text-secondary": "text-[#666666] dark:text-[#A0A0A0]",
+    "decoration-primary": "decoration-[#171717] dark:decoration-[#EDEDED]",
+    "decoration-secondary": "decoration-[#666666]/30 dark:decoration-[#A0A0A0]/30",
+    "text-link": "text-secondary underline underline-offset-5 decoration-secondary hover:(decoration-primary underline-offset-5 text-primary) duration-200",
+    "button-primary": "border border-[#EAEAEA] dark:border-[#2E2E2E] rounded-md hover:(border-[#C9C9C9] dark:border-[#454545] dark:bg-[#1A1A1A] bg-[#EBEBEB])",
   },
   presets: [
     presetWind4(),
