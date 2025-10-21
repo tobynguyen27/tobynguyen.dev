bf3fd15bbdb14d075f0f46c91c6f23fea896ce9f
Toby Nguyen <208221158+tobynguyen27@users.noreply.github.com>
2025-10-21 16:02:14 +0700

    Update home

--- uno.config.ts
+++ uno.config.ts
@@ -31,47 +31,31 @@ export default defineConfig({
   transformers: [transformerVariantGroup(), transformerAttributifyJsx()],
   theme: {
     colors: {
-      light: {
-        "bg-100": "hsla(0,0%,100%,1)",
-        "bg-200": "hsla(0,0%,98%,1)",
-        "gray-100": "hsla(0,0%,95%,1)",
-        "gray-200": "hsla(0,0%,92%,1)",
-        "gray-300": "hsla(0,0%,90%,1)",
-        "gray-400": "hsla(0,0%,92%,1)",
-        "gray-500": "hsla(0,0%,79%,1)",
-        "gray-600": "hsla(0,0%,66%,1)",
-        "gray-700": "hsla(0,0%,56%,1)",
-        "gray-800": "hsla(0,0%,49%,1)",
-        "gray-900": "hsla(0,0%,40%,1)",
-        "gray-1000": "hsla(0,0%,9%,1)",
-      },
-      dark: {
-        "bg-100": "hsla(0,0%,4%,1)",
-        "bg-200": "hsla(0,0%,0%,1)",
-        "gray-100": "hsla(0,0%,10%,1)",
-        "gray-200": "hsla(0,0%,12%,1)",
-        "gray-300": "hsla(0,0%,16%,1)",
-        "gray-400": "hsla(0,0%,18%,1)",
-        "gray-500": "hsla(0,0%,27%,1)",
-        "gray-600": "hsla(0,0%,53%,1)",
-        "gray-700": "hsla(0,0%,56%,1)",
-        "gray-800": "hsla(0,0%,49%,1)",
-        "gray-900": "hsla(0,0%,63%,1)",
-        "gray-1000": "hsla(0,0%,93%,1)",
-      },
+      "bg-100": "var(--bg-100)",
+      "bg-200": "var(--bg-200)",
+
+      "gray-100": "var(--gray-100)",
+      "gray-200": "var(--gray-200)",
+      "gray-300": "var(--gray-300)",
+      "gray-400": "var(--gray-400)",
+      "gray-500": "var(--gray-500)",
+      "gray-600": "var(--gray-600)",
+      "gray-700": "var(--gray-700)",
+      "gray-800": "var(--gray-800)",
+      "gray-900": "var(--gray-900)",
+      "gray-1000": "var(--gray-1000)",
     },
   },
   shortcuts: {
-    "navbar-item-text": "text-secondary hover:text-primary",
-    "bg-2": "bg-light-bg-200 dark:bg-dark-bg-200",
-    "bg-1": "bg-light-bg-100 dark:bg-dark-bg-100",
-    "text-primary": "text-light-gray-1000 dark:text-dark-gray-1000",
-    "text-secondary": "text-light-gray-900 dark:text-dark-gray-900",
-    "decoration-primary": "decoration-light-gray-1000 dark:decoration-dark-gray-1000",
-    "decoration-secondary": "decoration-light-gray-900/30 dark:decoration-dark-gray-900/30",
-    "text-link":
-      "text-secondary decoration-0.8 underline underline-offset-5 decoration-secondary hover:(decoration-primary underline-offset-5 text-primary) duration-200",
-    "button-primary":
-      "border border-light-gray-400 dark:border-dark-gray-400 rounded-md hover:(border-light-gray-500 dark:border-dark-gray-500 dark:bg-dark-gray-100 bg-light-gray-100)",
+    "text-primary": "text-gray-1000",
+    "text-secondary": "text-gray-900",
+
+    "bg-100": "bg-bg-100",
+    "bg-200": "bg-bg-200",
+
+    "navbar-item": "text-secondary hover:(text-primary cursor-pointer) duration-300 text-xl",
+
+    "underline-primary":
+      "underline decoration-offset-5 decoration-gray-1000/30 hover:(text-primary cursor-pointer decoration-gray-1000) duration-300",
   },
 });
