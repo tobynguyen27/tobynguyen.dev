5b90fd5748b1f891eb6a6a9f88a821ebe10c09dc
Toby Nguyen <208221158+tobynguyen27@users.noreply.github.com>
2025-11-07 14:00:04 +0700

    refactor blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,25 +2,11 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro";
-import rehypeAutolinkHeadings from "rehype-autolink-headings";
-import rehypeSlug from "rehype-slug";
 
 export default defineConfig({
     site: "https://tobynguyen.dev",
     output: "static",
-    integrations: [
-        UnoCSS(),
-        mdx({
-            syntaxHighlight: "shiki",
-            shikiConfig: { themes: { light: "catppuccin-latte", dark: "catppuccin-mocha" } },
-            gfm: true,
-            rehypePlugins: [
-                rehypeSlug,
-                [rehypeAutolinkHeadings, { behavior: "wrap", properties: { className: ["heading-anchor"] } }],
-            ],
-        }),
-        sitemap(),
-    ],
+    integrations: [UnoCSS(), mdx(), sitemap()],
     vite: { build: { assetsInlineLimit: 1024 } },
     server: { port: 3000 },
 });
