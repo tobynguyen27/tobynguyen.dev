7e72b0110522cee091a44b4ec546d3bdd16d2bdd
Toby Nguyen <208221158+tobynguyen27@users.noreply.github.com>
2026-03-16 21:09:55 +0700

    style: migrate to eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import antfu from "@antfu/eslint-config"
+
+export default antfu({
+    type: "app",
+
+    gitignore: true,
+
+    stylistic: {
+        indent: 4,
+        quotes: "double",
+    },
+    formatters: {
+        css: true,
+    },
+
+    vue: true,
+    typescript: true,
+    astro: true,
+    unocss: true,
+    toml: true,
+    jsonc: true,
+    yaml: {
+        overrides: {
+            "yaml/indent": ["error", 2],
+        },
+    },
+})
